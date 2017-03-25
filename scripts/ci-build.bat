c43ca409b894f144f42ea30df3d2fa47b50c124b
reinsteam <reinsteam@gmail.com>
2017-03-25 21:53:26 +0900

    Added VS 2010/2012 for test

--- scripts/ci-build.bat
+++ scripts/ci-build.bat
@@ -6,7 +6,11 @@ mkdir build
 
 cd build
 
-set msvc_gen_name=Visual Studio 9 2008
+set msvc_gen_name=Visual Studio
+
+if [x%PLATFORM:2008=%]==[x%PLATFORM%] set msvc_gen_name=%msvc_gen_name% 9 2008
+if [x%PLATFORM:2010=%]==[x%PLATFORM%] set msvc_gen_name=%msvc_gen_name% 10 2010
+if [x%PLATFORM:2010=%]==[x%PLATFORM%] set msvc_gen_name=%msvc_gen_name% 11 2012
 
 if not [x%PLATFORM:x64=%]==[x%PLATFORM%] set msvc_gen_name=%msvc_gen_name% Win64
 cmake .. -G "%msvc_gen_name%"
--- source/main.c
+++ source/main.c
@@ -3,5 +3,14 @@
 int main()
 {
     printf("Test !");
+
+#if defined (_TEST_DEFINE)
+    printf("_TEST_DEFINE defined");
+#endif
+
+#if defined(TEST_DEFINE_VALUE) && (TEST_DEFINE_VALUE == 1)
+    printf("TEST_DEFINE_VALUE==1 defined");
+#endif
+
     return 0;
 }
