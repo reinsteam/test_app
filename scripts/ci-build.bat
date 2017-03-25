8bc20d7d340f207a4ca2e50811f0c0a586632920
reinsteam <reinsteam@gmail.com>
2017-03-25 23:00:19 +0900

    Attempt to fix build script #2

--- scripts/ci-build.bat
+++ scripts/ci-build.bat
@@ -8,13 +8,15 @@ cd build
 
 set msvc_gen_name=Visual Studio
 
-if [x%PLATFORM:2008=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 9 2008 )
+if [x%PLATFORM:2008=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 9 2008 goto :build )
 else (
-    if [x%PLATFORM:2010=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 10 2010 )
+    if [x%PLATFORM:2010=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 10 2010 goto :build)
     else (
-        if [x%PLATFORM:2012=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 11 2012 )
+        if [x%PLATFORM:2012=%]==[x%PLATFORM%] ( set msvc_gen_name=%msvc_gen_name% 11 2012 goto :build)
     )
 )
+
+:build
 
 if not [x%PLATFORM:x64=%]==[x%PLATFORM%] set msvc_gen_name=%msvc_gen_name% Win64
 cmake .. -G "%msvc_gen_name%"
