d6339bcbddfc3ee3456764ebddd11d65b4373bd6
reinsteam <reinsteam@gmail.com>
2017-04-16 01:29:50 +0900

    Report compiler version

--- source/main.c
+++ source/main.c
@@ -27,14 +27,22 @@ RESTORE_WARNINGS_MSVC()
 
 int main(void)
 {
-    printf_custom("Test !");
+    printf_custom("Test !\n");
 
 #if defined (_TEST_DEFINE)
-    printf_custom("_TEST_DEFINE defined");
+    printf_custom("_TEST_DEFINE defined\n");
 #endif
 
 #if defined(TEST_DEFINE_VALUE) && (TEST_DEFINE_VALUE == 1)
-    printf_custom("TEST_DEFINE_VALUE==1 defined");
+    printf_custom("TEST_DEFINE_VALUE==1 defined\n");
+#endif
+
+#ifdef __clang__
+    printf_custom("__clang__ defined, __clang_major__ : %d, __clang_minor__ : %d\n", __clang_major__, __clang_minor__);
+#endif
+
+#ifdef __GNUC__
+    printf_custom("__GNUC__ defined, __GNUC__ : %d, __GNUC_MINOR__ : %d, __GNUC_PATCHLEVEL__ : %d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
 #endif
 
     return 0;
